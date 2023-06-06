f172d011bb2ab080b229e787f0363b195f01bef0
loroberge <lroberge@tulane.edu>
2023-06-06 17:22:15 -0500

    Add new __init__ and component files to repo

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 15:21:55 2023
+
+@author: LaurentRoberge
+"""
+
+from .sediment_property_concentration_tracker import ConcentrationTracker
+
+__all__ = ["ConcentrationTracker"]
---
+++ sediment_property_concentration_tracker.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 31 11:41:20 2023
+
+@author: LaurentRoberge
+"""
+
+import numpy as np
+
+from landlab import Component, LinkStatus
+from landlab.grid.mappers import map_value_at_max_node_to_link
+
+
+class ConcentrationTracker(Component):
+
+    """This component tracks the concentration of any user-defined property of
+    sediment using a mass balance approach in which the concentration :math:`C`
+    is calculated as:
+
+    .. math::
+
+        ∂CH/∂t = [-(∂q_x C_x)/∂x - (∂q_y C_y)/∂y] + PH + DH
+        
+    where :math:`H` is sediment depth, :math:`q_x` and :math:`q_y` are sediment
+    fluxed in the x and y directions, :math:`P` is the local production rate,
+    :math:`D` is the local decay rate.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from landlab import RasterModelGrid
+    >>> from landlab.components import ExponentialWeatherer
+    >>> from landlab.components import DepthDependentDiffuser
+    >>> from landlab.components import ConcentrationTracker
+    >>> ... SIMPLE EXAMPLE
+    
+    Now for a 1-D normal fault scarp undergoing hillslope diffusion:
+
+    >>> ... EXAMPLE  
+    
+    Now, a 2-D landscape with stream channels and hillslopes.
+
+    >>> ... EXAMPLE  
+
+    References
+    ----------
+    **Required Software Citation(s) Specific to this Component**
+
+    CITATION
+
+    """
+
+    _name = "ConcentrationTracker"
+
+    _unit_agnostic = True
+
+    _cite_as = """
+    CITATION
+    """
+
+    _info = {
+        "bedrock__elevation": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "m",
+            "mapping": "node",
+            "doc": "elevation of the bedrock surface",
+        },
+        "soil__depth": {
+            "dtype": float,
+            "intent": "inout",
+            "optional": False,
+            "units": "m",
+            "mapping": "node",
+            "doc": "Depth of soil or weathered bedrock",
+        },
+        "soil__flux": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "m^2/yr",
+            "mapping": "link",
+            "doc": "flux of soil in direction of link",
+        },
+        "soil_production__rate": {
+            "dtype": float,
+            "intent": "in",
+            "optional": False,
+            "units": "m/yr",
+            "mapping": "node",
+            "doc": "rate of soil production at nodes",
+        },
+        "topographic__elevation": {
+            "dtype": float,
+            "intent": "inout",
+            "optional": False,
+            "units": "m",
+            "mapping": "node",
+            "doc": "Land surface topographic elevation",
+        },
+        "topographic__slope": {
+            "dtype": float,
+            "intent": "out",
+            "optional": False,
+            "units": "m/m",
+            "mapping": "link",
+            "doc": "gradient of the ground surface",
+        },
+    }
+
+    def __init__(self, grid, local_production_rate=0, local_decay_rate=0):
+        """
+        Parameters
+        ----------
+        grid: ModelGrid
+            Landlab ModelGrid object
+        local_production_rate: float
+            Rate of local production, kg**3/yr
+        local_decay_rate: float
+            Rate of local decay, kg**3/yr
+        """
+        super().__init__(grid)
+        # Store grid and parameters
+
+        self._P = local_production_rate
+        self._D = local_decay_rate
+        
+        # NOT SURE WHERE TO PUT THIS.... OUTPUT?
+        self._soil__depth_old = np.zeros(grid.number_of_nodes)
+        self._soil__depth_old += self._grid.at_node["soil__depth"]
+
+        # get reference to inputs
+        self._elev = self._grid.at_node["topographic__elevation"]
+        self._soil__depth = self._grid.at_node["soil__depth"]
+        self._flux = self._grid.at_node["sediment__flux"]
+        self._flux = self._grid.at_link["soil__flux"]
+
+        # create outputs if necessary and get reference.
+        self.initialize_output_fields()
+        # self._slope = self._grid.at_link["topographic__slope"]
+        # self._bedrock = self._grid.at_node["bedrock__elevation"]
+
+    def concentration(self, dt):
+        """Calculate change in concentration for a time period 'dt'.
+
+        Parameters
+        ----------
+
+        dt: float (time)
+            The imposed timestep.
+        """
+                
+        # Define soil depth at current time
+        C_old = self._grid.at_node['Sm__concentration']
+        
+        # Map concentration from nodes to links (following soil flux direction)
+        self._grid.at_link['C'] = map_value_at_max_node_to_link(
+            self._grid,'topographic__elevation','Sm__concentration'
+            )
+        
+        # Calculate QC at links (sediment flux times concentration)
+        self._grid.at_link['QC'] = (self._grid.at_link['soil__flux'] *
+                                    self._grid.at_link['C']
+                                    )
+        
+        # Replace nan values with zeros (DOUBLE CHECK IF THIS IS NECESSARY)
+        self._grid.at_link['QC'][np.isnan(self._grid.at_link['QC'])] = 0
+        
+        # Calculate flux concentration divergence
+        dQCdx = self._grid.calc_flux_div_at_node(self._grid.at_link['QC'])
+        
+        # Calculation other components of mass balance equation
+        C_local = C_old * (self._soil__depth_old/self._soil__depth)
+        Production = (dt*P/2) * (self._soil__depth_old/self._soil__depth + 1)
+        Decay = (dt*D/2) * (self._soil__depth_old/self._soil__depth + 1)
+        
+        mg.at_node['Sm__concentration'] = C_local + (dt/self._soil__depth) * (- dQCdx) + Production - Decay
+        
+        # Define concentration
+        C_out = mg.at_node['Sm__concentration']
+        C_old = mg.at_node['Sm__concentration']
+        H_old = H_new
+
+
+
+
+
+
+        # update soil thickness
+        self._grid.at_node["soil__depth"][:] = (
+            self._grid.at_node["topographic__elevation"]
+            - self._grid.at_node["bedrock__elevation"]
+        )
+
+        # Calculate soil depth at links.
+        H_link = self._grid.map_value_at_max_node_to_link(
+            "topographic__elevation", "soil__depth"
+        )
+
+        # Calculate gradients
+        slope = self._grid.calc_grad_at_link(self._elev)
+        slope[self._grid.status_at_link == LinkStatus.INACTIVE] = 0.0
+
+        # Calculate flux
+        self._flux[:] = (
+            -self._K
+            * slope
+            * self._soil_transport_decay_depth
+            * (1.0 - np.exp(-H_link / self._soil_transport_decay_depth))
+        )
+
+        # Calculate flux divergence
+        dqdx = self._grid.calc_flux_div_at_node(self._flux)
+
+        # Calculate change in soil depth
+        dhdt = self._soil_prod_rate - dqdx
+
+        # Calculate soil depth at nodes
+        self._depth[self._grid.core_nodes] += dhdt[self._grid.core_nodes] * dt
+
+        # prevent negative soil thickness
+        self._depth[self._depth < 0.0] = 0.0
+
+        # Calculate bedrock elevation
+        self._bedrock[self._grid.core_nodes] -= (
+            self._soil_prod_rate[self._grid.core_nodes] * dt
+        )
+
+        # Update topography
+        self._elev[self._grid.core_nodes] = (
+            self._depth[self._grid.core_nodes] + self._bedrock[self._grid.core_nodes]
+        )
+
+    def run_one_step(self, dt):
+        """
+
+        Parameters
+        ----------
+        dt: float (time)
+            The imposed timestep.
+        """
+
+        self.concentration(dt)
